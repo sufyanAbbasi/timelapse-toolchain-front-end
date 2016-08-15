392583bb0f8c27c180cba7ea58f6d9a6404e03ce
sufyanAbbasi <sufy.abbasi@gmail.com>
2016-08-15 17:37:48 -0400

    Save and Share button works, somewhat serialized object for server

--- js/result-js.js
+++ js/result-js.js
@@ -11,13 +11,17 @@ function initializeCustomization(){
 	  event.stopPropagation();
 	});
 
+	dictionary['title'] = "";
+	dictionary['description'] = "";
+
 }
 
 function setDataset(dataset){
 	dictionary[dataSetIndex] = {};
 	dictionary[dataSetIndex]['original'] = $.extend(true, {}, dataset);
 	var formState = {};
-	$('#drop-down-list').append('<option value='+ dataSetIndex +'>'+ dataset.name +'</option>');
+	var globalFormState = {};
+	$('#drop-down-list').prepend('<option value='+ dataSetIndex +'>'+ dataset.name +'</option>');
 
 	var value = rgbToHex(parseInt(dataset.rgba[0]*256), parseInt(dataset.rgba[1]*256), parseInt(dataset.rgba[1]*256));
 	$('input[name=set-color]')[0].value = value;
@@ -29,19 +33,22 @@ function setDataset(dataset){
 	formState['size'] = {'toggle' : false, 'value' : dataset.pointSize};
 
 	//CHANGE THIS TO ACTUAL VALUE
-	$('input[name=set-span]')[0].value = (dataset.duration) ? convertDurationToPercent(dataset.duration) : 100;
-	setVal('span-val', dataset.duration || 100);
-	formState['span'] = {'toggle' : false, 'value' : (dataset.duration) ? convertDurationToPercent(dataset.duration) : 100}
+	var duration = (dataset.duration) ? convertDurationToPercent(dataset.duration) : 100;
+	$('input[name=set-span]')[0].value = duration;
+	setVal('span-val', duration);
+	formState['span'] = {'toggle' : false, 'value' : duration};
 	//
 
 	$('input[name=set-hardness]')[0].value = dataset.hardness;
 	setVal('hardness-val', dataset.hardness);
 	formState['hardness'] = {'toggle' : false, 'value' : dataset.hardness};
 
-	formState['blend'] = {'toggle' : false, 'value' : 'additive'};
+	globalFormState['blend'] = {'toggle' : false, 'value' : 'additive'};
 
 	dictionary[dataSetIndex]['formState'] = formState;
+	dictionary['globalFormState'] = globalFormState;
 
+	$('select')[0].value = dataSetIndex;
 	dataSetIndex++;
 	$('input').prop('disabled','');
 	$('textarea').prop('disabled','');
@@ -51,7 +58,7 @@ function switchDataset(index){
 	loadFormState(dictionary[index]['formState']);
 }
 
-function updateAttributes(input){
+function updatePointAttributes(input){
 	var attributeName = input.name.split('-')[1];
 	var value = input.value;
 	var currentIndex = getCurrentIndex();
@@ -87,7 +94,23 @@ function updateAttributes(input){
 	setData(currentIndex, attributeName, value);
 }
 
-function toggleAttribute(input){
+function updateGlobalAttributes(input){
+	var attributeName = input.name.split('-')[1];
+	var value = input.value;
+	dictionary['globalFormState'][attributeName].toggle = true;
+	dictionary['globalFormState'][attributeName].value = value;
+
+	switch(attributeName){
+		case 'blend':
+			setBlendFunc($('input[name=set-blend]:checked').val(), gl);
+			break;
+		default:
+			console.log('cannot identify: ' + attributeName);
+			return;
+	}
+}
+
+function togglePointAttribute(input){
 	var attributeName = input.name.split('-')[1];
 	var value = $('input[name=set-'+ attributeName +']')[0].value;
 	var currentIndex = getCurrentIndex();
@@ -110,10 +133,6 @@ function toggleAttribute(input){
 				attributeName = 'hardness';
 				value = parseFloat(value);
 				break;
-			case 'blend':
-				setBlendFunc($('input[name=set-blend]:checked').val(), gl);
-				return;
-				break;
 			default:
 				console.log('cannot identify: ' + attributeName);
 				return;
@@ -133,19 +152,37 @@ function toggleAttribute(input){
 			case 'hardness':
 				attributeName = 'hardness';
 				break;
-			case 'blend':
-				setBlendFunc('additive', gl);
-				return;
-				break;
 			default:
 				console.log('cannot identify: ' + attributeName);
 				return;
 		}
 
 		value = originalDataset[attributeName];
-		console.log(value);
 	}
 	setData(currentIndex, attributeName, value);
+}
+
+function toggleGlobalAttribute(input){
+	var attributeName = input.name.split('-')[1];
+	var value = $('input[name=set-'+ attributeName +']')[0].value;
+	dictionary['globalFormState'][attributeName].toggle = input.checked;
+	if(input.checked){
+		switch(attributeName){
+			case 'blend':
+				setBlendFunc($('input[name=set-blend]:checked').val(), gl);
+				break;
+			default:
+				console.log('cannot identify: ' + attributeName);
+		}
+	}else{
+		switch(attributeName){
+			case 'blend':
+				setBlendFunc('additive', gl);
+				break;
+			default:
+				console.log('cannot identify: ' + attributeName);
+		}
+	}
 }
 
 function getCurrentIndex(){
@@ -231,18 +268,21 @@ function reinitializeTimeSlider(){
 	timeSlider = new TimeSlider(timeSliderOptions);
 }
 
+function setTitle(input){
+	dictionary['title'] = input.value;
+}
+
+function setDescription(input){
+	dictionary['description'] = input.value;
+}
+
 function loadFormState(formState){
 	for(var key in formState){
 		$('input[name=toggle-'+ key + ']')[0].checked = formState[key].toggle;
-		if(key != 'blend'){
-			$('input[name=set-'+ key + ']')[0].value = formState[key].value;
-		}else{
-			var input = $('input[name=set-'+ key + '][value='+ formState[key].value + ']')[0];
-			input.checked = true;
-			setBlendFunc(input.value, gl);
-
+		$('input[name=set-'+ key + ']')[0].value = formState[key].value;
+		if(key != 'color'){
+			setVal(key + "-val", formState[key].value)
 		}
-		
 	}
 }
 
@@ -253,8 +293,75 @@ function preview(){
 	map.setCenter(mapOptions.center);
 }
 
-function submit(){
 
+//baseurl.com/projects/project_name/edit 
+//
+function save(){
+	packageAndSendData();
+	var exportDiv = [];
+	exportDiv.push('<div class="dark-background">');
+	exportDiv.push('<div class="tool-window centerXY" id="share-window">');
+	exportDiv.push('<div class="x-button x-small"></div>');
+	exportDiv.push('<div class="share-title">');
+	exportDiv.push('<h4>Link:</h4>');
+	exportDiv.push('<textarea id="link" rows="1" readonly="true" wrap="off">www.customtimelapse.com/#test</textarea>');
+	exportDiv.push('</div>');
+	exportDiv.push('<div class="share-title">');
+	exportDiv.push('<h4>Embed:</h4>');
+	exportDiv.push('<textarea id="embed" rows="5" readonly="true" wrap="soft">');
+	exportDiv.push('&lt;iframe src=&quot;www.customtimelapse.com/#test&quot; style=&quot;width:100%; height:100%;&quot; marginwidth=&quot;0&quot; marginheight=&quot;0&quot; frameborder=&quot;0&quot; vspace=&quot;0&quot; hspace=&quot;0&quot;&gt;&lt;/iframe&gt;</textarea>');
+	exportDiv.push('</div>');
+	exportDiv.push('<div class="share-title"><a id="download" href="" download><div class="button" id="download-button">Download Source Files</div></a></div>');
+	exportDiv.push('</div>');
+	exportDiv.push('</div>');
+
+	var exportDiv = $(exportDiv.join(''));
+	var xButton = $(exportDiv).find('.x-button');
+	xButton.click(function(){
+		$('.dark-background').remove();
+	});
+	$('#map-tools').after(exportDiv);
+
+
+	//$('body').append(shareScreen);
+
+}
+
+function packageAndSendData(){
+	var data = {};
+	data['title'] = dictionary.title;
+	data['description'] = dictionary.description;
+	data['map'] = {};
+	data['map']['zoom'] = mapOptions.zoom;
+	data['map']['center'] = [mapOptions.center.lat(), mapOptions.center.lng()];
+	data['timeSlider'] = timeSliderOptions;
+	data['blend'] = dictionary.globalFormState.blend.value;
+	data['datasets'] = [];
+	for(var i = 0; i < datasets.length; i++){
+		var obj = $.extend(true, {}, datasets[i]);
+		delete obj.arrayBuffer;
+		delete obj.attributeLocation;
+		delete obj.count;
+		delete obj.data;
+		delete obj.dataLoaded;
+		delete obj.program;
+		delete obj.attributeLocation;
+        delete obj.timeLocation;
+        delete obj.u_ColorLocation;
+        delete obj.u_PointSizeLocation;
+        delete obj.u_HardnessLocation;
+        delete obj.u_MapMatrixLocation;
+        delete obj.u_TimeBoundsLocation;
+        data['datasets'].push(obj);
+	}
+	console.log(JSON.stringify(data));
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", '', true);
+	xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+	xhr.send(JSON.stringify(data));
+	xhr.onloadend = function () {
+		console.log("Update recieved.");
+	};
 }
 
 $(initializeCustomization);
